166c9327980170913f98dbc3ebff11d75dd316d6
Promit7473 <108547743+promit7473@users.noreply.github.com>
2024-08-21 12:30:13 +0600

    build

--- 4.Using ROS2 in Python/workspaces/ros2_py_ws/build/udemy_ros2_pkg/cmake_install.cmake
+++ 4.Using ROS2 in Python/workspaces/ros2_py_ws/build/udemy_ros2_pkg/cmake_install.cmake
@@ -43,6 +43,30 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/udemy_ros2_pkg/environment" TYPE FILE FILES "/home/mhpromit7473/Documents/GitHub/ROS2-Robotics-Developer-Course/4.Using ROS2 in Python/workspaces/ros2_py_ws/build/udemy_ros2_pkg/ament_cmake_environment_hooks/pythonpath.sh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/udemy_ros2_pkg/environment" TYPE FILE FILES "/home/mhpromit7473/Documents/GitHub/ROS2-Robotics-Developer-Course/4.Using ROS2 in Python/workspaces/ros2_py_ws/build/udemy_ros2_pkg/ament_cmake_environment_hooks/pythonpath.dsv")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/local/lib/python3.10/dist-packages/scripts-0.0.0-py3.10.egg-info" TYPE DIRECTORY FILES "/home/mhpromit7473/Documents/GitHub/ROS2-Robotics-Developer-Course/4.Using ROS2 in Python/workspaces/ros2_py_ws/build/udemy_ros2_pkg/ament_cmake_python/scripts/scripts.egg-info/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/local/lib/python3.10/dist-packages/scripts" TYPE DIRECTORY FILES "/home/mhpromit7473/Documents/GitHub/ROS2-Robotics-Developer-Course/4.Using ROS2 in Python/workspaces/ros2_py_ws/src/udemy_ros2_pkg/scripts/" REGEX "/[^/]*\\.pyc$" EXCLUDE REGEX "/\\_\\_pycache\\_\\_$" EXCLUDE)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  execute_process(
+        COMMAND
+        "/usr/bin/python3" "-m" "compileall"
+        "/home/mhpromit7473/Documents/GitHub/ROS2-Robotics-Developer-Course/4.Using ROS2 in Python/workspaces/ros2_py_ws/install/udemy_ros2_pkg/local/lib/python3.10/dist-packages/scripts"
+      )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/mhpromit7473/Documents/GitHub/ROS2-Robotics-Developer-Course/4.Using ROS2 in Python/workspaces/ros2_py_ws/build/udemy_ros2_pkg/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/udemy_ros2_pkg")
 endif()
 
